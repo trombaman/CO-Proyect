bbd2c026ce8519573e59aa14c66c3a7be3fbc8c7
BrianDRC <Brarojasc@gmail.com>
2017-03-23 14:01:47 -0600

    Add files via upload

---
+++ Base.py
@@ -0,0 +1,20 @@
+#prueba
+
+def suma():
+    x = int(input("Digite un número: \n"))
+    y = int(input("Digite otro número: \n"))
+    print("La suma",x,"+",y ,"es", x+y)
+
+def resta():
+    x = int(input("Digite un número: \n"))
+    y = int(input("Digite otro número: \n"))
+    print("La resta", x, "-", y, "es", x - y)
+
+def menu():
+    print("1 suma / 2 resta")
+    opc = int(input("elija uan opcion: "))
+    if opc == 1:
+        suma()
+    elif opc == 2:
+        resta()
+menu()
